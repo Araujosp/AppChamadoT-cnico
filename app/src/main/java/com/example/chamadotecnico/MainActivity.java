0d31f09345499ccc01273bae2524d7720c173f51
GABRIEL ARAUJO JANUARIO DE FREITAS <25254286@educ123.sp.senai.br>
2026-09-17 10:14:05 -0300

    First commit

---
+++ app/src/main/java/com/example/chamadotecnico/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.chamadotecnico;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.appbar.MaterialToolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        MaterialToolbar toolbar = findViewById(R.id.toolbarTop); //pegamos o material xml e linkamos ao java
+        setSupportActionBar(toolbar); // esse toolbar vai ser usado para navegar entre as telas
+
+
+    }
+
+    //definindo as opções do menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_top, menu);
+        return  true;
+    }
+
+
+}
